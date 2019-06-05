092d4892cdd77b9ef4dab43f4f9a7f26c80d443b
Michael D <miketdennison@gmail.com>
2019-06-05 16:48:04 -0500

    renamed files

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,23 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-document.getElementsByClassName("img1")[0].setAttribute("src", `assets/images/dice${randomNumber1}.png`);
-document.getElementsByClassName("img2")[0].setAttribute("src", `assets/images/dice${randomNumber2}.png`);
+
+var randomImage1 = `assets/images/dice${randomNumber1}.png`;
+var randomImage2 = `assets/images/dice${randomNumber2}.png`;
+
+var img1 = document.getElementsByClassName("img1")[0];
+var img2 = document.getElementsByClassName("img2")[0];
+
+img1.setAttribute("src", randomImage1);
+img2.setAttribute("src", randomImage2);
+
+var h1 = document.getElementsByTagName("h1")[0];
 
 if(randomNumber1 > randomNumber2) {
-    document.getElementsByTagName("h1")[0].innerHTML = "&#x1F6A9; Player 1 Wins"
+    h1.innerHTML = "&#x1F6A9; Player 1 Wins"
 }
 else if(randomNumber1 < randomNumber2) {
-    document.getElementsByTagName("h1")[0].innerHTML = "Player 2 Wins &#x1F6A9;"
+    h1.innerHTML = "Player 2 Wins &#x1F6A9;"
 }
 else {
-    document.getElementsByTagName("h1")[0].innerHTML = "Draw!"
+    h1.innerHTML = "Draw!"
 }
